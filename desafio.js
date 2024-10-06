92309ca63395b8988f29f60bed4686c0bc2a8714
Peronny Segundo <peronnysegundo@gmail.com>
2024-10-06 04:33:17 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,13 +1,30 @@
 const lista = [[1,2,3], [4,5,6], [7,8], [9,10,11,12], [-13]]
+const lista1 = [[1,2,3,-13], [4,5,6,-13], [7,8,-13], [9,10,11,12,-13], [-13]]
 funcao = (lista) => {
     let menor = Infinity;
-    for (indexLista = 0; indexLista < lista.length; indexLista++){
-        for(indexListaMenor = 0; indexListaMenor < lista.length; indexListaMenor++){
-            if(lista[indexLista][indexListaMenor] < menor){
-                menor = lista[indexLista][indexListaMenor]
+    for (index in lista){
+        for(indexMenor in lista[index]){
+            if(lista[index][indexMenor] < menor){
+                menor = lista[index][indexMenor]
             }
         }
     }
-    return menor;
+    var contador = -1;
+    var numeroDeItens = -1; //não to conseguindo usar o lenght
+    for (index in lista){
+        numeroDeItens++
+        for (indexMenor in lista[index]){
+            if (lista[index][indexMenor] == menor){
+                contador++
+            }
+        }
+    }
+    if (contador == numeroDeItens){
+        return(menor)
+    }
+    else{
+        return(-1)
+    }
 }
 console.log(funcao(lista))
+console.log(funcao(lista1))
